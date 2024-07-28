d1bfdfc216d92602e5264bdc7a4aab5509fae760
ridho008 <ciuyy12@gmail.com>
2024-07-28 22:22:18 +0700

    add keranjang, removeitem

--- app/Livewire/Transaction/Actions.php
+++ app/Livewire/Transaction/Actions.php
@@ -2,13 +2,52 @@
 
 namespace App\Livewire\Transaction;
 
+use App\Models\Customer;
 use App\Models\Menu;
 use Livewire\Component;
 
 class Actions extends Component
 {
+    public $search;
+
+    public $items = [];
+
+    // menambahkan menu kedalam tampilan (keranjang)
+    public function addItemMenu(Menu $menu) {
+        if(isset($this->items[$menu->name]) ) {
+            $item = $this->items[$menu->name];
+            $this->items[$menu->name] = [
+            'qty' => $item['qty'] + 1,
+            'price' => $item['price'] + $menu->price
+        ];
+        } else {
+            $this->items[$menu->name] = [
+            'qty' => 1,
+            'price' => $menu->price
+        ];
+        }
+    }
+
+    public function removeItemMenu($key) {
+        $item =$this->items[$key];
+
+        if($item['qty'] > 1) {
+            $hargaSatuan = $item['price'] / $item['qty'];
+            $qtyTerbaru = $item['qty'] - 1;
+
+            $this->items[$key]['qty'] = $qtyTerbaru;
+            $this->items[$key]['price'] = $hargaSatuan * $qtyTerbaru;
+
+        }
+    }
+
     public function render()
     {
-        return view('livewire.transaction.actions', ['menus' => Menu::get()->groupBy('type')]);
+        return view('livewire.transaction.actions', ['menus' => Menu::when($this->search, function($menu) {
+            $menu->where('name', 'like', '%'. $this->search)
+            ->orWhere('type', 'like', '%'. $this->search);
+        })->get()->groupBy('type'),
+        'customers' => Customer::pluck('name', 'id')
+        ]);
     }
 }
--- resources/views/livewire/transaction/actions.blade.php
+++ resources/views/livewire/transaction/actions.blade.php
@@ -2,28 +2,75 @@
     <div class="grid grid-cols-2 gap-6">
         <div class="card m-3 bg-white card-divider">
             <div class="card-body ">
-                <input type="search" class="input input-bordered" placeholder="Search menu" wire:model="search">
+                <input type="search" class="input input-bordered" placeholder="Search menu" wire:model.live="search">
             </div>
             <div>
+                {{-- @php
+                    dd($menus);
+                @endphp --}}
                 @foreach ($menus as $type => $menu)
                     <div class="card p-2">
-                        <h3 class="text-2xl font-bold capitalize">{{ $type }}</h3>
-                        <div class="grid grid-cols-4 gap-2">
-                            @foreach ($menu as $item)
-                                <div class="avatar">
-                                    <div class=" w-full rounded-md">
-                                        <img src="{{ $item->images }}" alt="{{ $item->images }}">
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
+                        @if ($menu->isEmpty())
+                            <div class="card">
+                                <span class="text-red-400">empty!</span>
+                            </div>
+                        @else
+                            <h3 class="text-2xl font-bold capitalize">{{ $type }}</h3>
+                            <div class="grid grid-cols-4 gap-2">
+                                @foreach ($menu as $item)
+                                    <button wire:click="addItemMenu({{ $item->id }})" class="avatar">
+                                        <div class=" w-full rounded-md">
+                                            <img src="{{ $item->images }}" alt="{{ $item->images }}">
+                                        </div>
+                                    </button>
+                                @endforeach
+                            </div>
+                        @endif
                     </div>
                 @endforeach
             </div>
         </div>
-        <div class="card m-3 bg-slate-700">
-            <div class="card-body">
-                <h4>Form</h4>
+        <div class="card m-3 bg-slate-200">
+            <div class="card-body space-y-4">
+                <h3 class="card-title">Detail Transaction</h3>
+                @json($items)
+                <div class="table-wrapper">
+                    <table class="table">
+                        <thead>
+                            <th>Name Menu</th>
+                            <th>Qty</th>
+                            <th>Price</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($items as $key => $val)
+                                <tr>
+                                    <td>{{ $key }}</td>
+                                    <td>{{ $val['qty'] }}</td>
+                                    <td>{{ Number::format($val['price']) }}</td>
+                                    <td>
+                                        <button class="btn btn-xs"
+                                            wire:click="removeItemMenu(`{{ $key }}`)">-</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <select name="" class="select select-bordered" id="" wire:model="customer_id">
+                    <option value="">Choose Customers</option>
+                    @foreach ($customers as $id => $name)
+                        <option value="{{ $id }}">{{ $name }}</option>
+                    @endforeach
+                </select>
+                <textarea name="" id="" class="textarea textarea-bordered" placeholder="Makan ditempat/Bungkus"
+                    wire:model="desc" cols="30" rows="3"></textarea>
+                <div class="card-actions justify-between">
+                    <div class="flex flex-col">
+                        <div class="text-xs">Total Price</div>
+                        <div class="card-title">Rp.0</div>
+                    </div>
+                    <button class="btn btn-primary">Save</button>
+                </div>
             </div>
         </div>
     </div>
